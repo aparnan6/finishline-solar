8e0b58ded8efbf8101f3fc1b16bc172eb0a15ca8
aparna natarajan <aparnan@terpmail.umd.edu>
2017-11-07 15:16:33 -0500

    added intro components

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var http = require('http');
+var fs = require("fs");
+ 
+http.createServer(function(request, response) {
+	if(request.url === "/"){
+		sendFileContent(response, "index.html", "text/html");
+	}
+}).listen(3000);
+
+function sendFileContent(response, fileName, contentType){
+	fs.readFile(fileName, function(err, data){
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
